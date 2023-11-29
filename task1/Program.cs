9ca3689d2d5a990696792615b49889917f818589
Skizex <justskizex@gmail.com>
2023-11-29 16:05:42 +0400

    start

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элемент числа: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
